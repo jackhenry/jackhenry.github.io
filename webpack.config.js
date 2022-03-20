ca0b5852e874cdfacac1828249a065eacc881020
Jack Henry <jack@jackerickson.net>
2022-03-20 05:31:34 +0000

    Linted webpack config and removed posts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    target: 'web',
-    module: {
-        rules: [
-          {
-            test: /\.(js)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
+  entry: './src/index',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  target: 'web',
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-          {
-              test: /\.css$/i,
-              use: ['style-loader', 'css-loader']
-          }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.html']
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'public')
         },
-        hot: true,
-        compress: true,
-        port: 9000
-    }
-}
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.html'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    hot: true,
+    compress: true,
+    port: 9000,
+  },
+};
