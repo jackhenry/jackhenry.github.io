653a04f2dd414bcc7b60bab4dc1099ba700a7ed7
Jack Henry <jack@jackerickson.net>
2022-03-20 05:32:30 +0000

    Bundled new pages build.

--- src/posts.js
+++ src/posts.js
@@ -1,32 +1,18 @@
 /**
  * DO NOT DELETE OR MODIFY FILE.
  * This file is injected during the preprocessing stage.
-*/
+ */
 
-export default [
-    {
-            fdate: '2019-08-11',
-            date: 'Sun Aug 11 2019 08:45:30 GMT-0500 (Central Daylight Time)',
-            author: 'Jack Henry',
-            title: 'Test Markdown Document',
-            path: './posts/post0.html',
-        },{
-            fdate: '2011-08-11',
-            date: 'Thu Aug 11 2011 08:45:30 GMT-0500 (Central Daylight Time)',
-            author: 'Jack Henry',
-            title: 'Elit eget gravida cum sociis natoque penatibus et.',
-            path: './posts/post1.html',
-        },{
-            fdate: '2011-07-11',
-            date: 'Mon Jul 11 2011 08:45:30 GMT-0500 (Central Daylight Time)',
-            author: 'Jack Henry',
-            title: 'Egestas fringilla phasellus faucibus scelerisque.',
-            path: './posts/post2.html',
-        },{
-            fdate: '2009-06-15',
-            date: 'Mon Jun 15 2009 08:45:22 GMT-0500 (Central Daylight Time)',
-            author: 'Jack Henry',
-            title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
-            path: './posts/post3.html',
-        }
-    ]
+export default [{
+    fdate: '2022-03-20',
+    date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
+    author: 'мырзам',
+    title: 'менің күнім',
+    path: './posts/post0.html',
+}, {
+    fdate: '2019-08-11',
+    date: 'Sun Aug 11 2019 13:45:30 GMT+0000 (Coordinated Universal Time)',
+    author: 'Jack Henry',
+    title: 'Test Markdown Document',
+    path: './posts/post1.html',
+}];
