56ecccd4f52f774fa77a9432934b712f8af97fd8
Jack Henry <jack@jackerickson.net>
2022-03-20 03:09:34 +0000

    Setup eslint and linted files.

--- generate.js
+++ generate.js
@@ -5,12 +5,12 @@ const moment = require('moment');
 const handlebars = require('handlebars');
 
 async function fetchMarkdownFiles() {
-    const mdFileNames = await readdir('./src/content/');
-    const fetchAllFileContents = await mdFileNames.map(async filename => await readFile(`./src/content/${filename}`, {
-        encoding: 'utf-8'
-    }));
-    const fileContents = await Promise.all(fetchAllFileContents);
-    return fileContents;
+  const mdFileNames = await readdir('./src/content/');
+  const fetchAllFileContents = await mdFileNames.map(async (filename) => await readFile(`./src/content/${filename}`, {
+    encoding: 'utf-8',
+  }));
+  const fileContents = await Promise.all(fetchAllFileContents);
+  return fileContents;
 }
 
 /**
@@ -19,52 +19,52 @@ async function fetchMarkdownFiles() {
  * @returns An object containing markdown rendered as html, title, date, and author
  */
 async function parseMarkdownFiles() {
-    const fileContents = await fetchMarkdownFiles();
-    const data = fileContents.map(content => {
-        /**
+  const fileContents = await fetchMarkdownFiles();
+  const data = fileContents.map((content) => {
+    /**
          * Each markdown file has a yaml header. The beginning of the yaml
          * is marked by three dashes and the end of yaml is denoted by three more dashes.
          * Anything between those two markers are expected to be valid YAML
          */
-        const splitContent = content.split('---');
-        const yamlFrontMatter = splitContent[1];
-        const markdown = splitContent[2];
-        const html = marked(markdown);
-        const metadata = yaml.load(yamlFrontMatter);
-        // Format the date
-        const parsedDate = moment.utc(metadata['date']);
-        const formattedDate = parsedDate.format('YYYY-MM-DD');
-        return { html: html, fdate: formattedDate, ...metadata }
-    });
+    const splitContent = content.split('---');
+    const yamlFrontMatter = splitContent[1];
+    const markdown = splitContent[2];
+    const html = marked(markdown);
+    const metadata = yaml.load(yamlFrontMatter);
+    // Format the date
+    const parsedDate = moment.utc(metadata.date);
+    const formattedDate = parsedDate.format('YYYY-MM-DD');
+    return { html, fdate: formattedDate, ...metadata };
+  });
 
-    // Sort the dates to the most recent post is first
-    data.sort((a, b) => {
-        const adate = moment(a['date']);
-        const bdate = moment(b['date']);
+  // Sort the dates to the most recent post is first
+  data.sort((a, b) => {
+    const adate = moment(a.date);
+    const bdate = moment(b.date);
 
-        return bdate.diff(adate);
-    })
+    return bdate.diff(adate);
+  });
 
-    return data;
+  return data;
 }
 
 async function createHtmlFiles() {
-    const source = await readFile('./templates/post.html', 'utf-8');
-    const template = handlebars.compile(source);
+  const source = await readFile('./templates/post.html', 'utf-8');
+  const template = handlebars.compile(source);
 
-    const data = await parseMarkdownFiles();
-    const htmlList = data.map(obj => template({ post: obj }));
+  const data = await parseMarkdownFiles();
+  const htmlList = data.map((obj) => template({ post: obj }));
 
-    for (let index = 0; index < data.length; index++) {
-        const html = htmlList[index];
-        const htmlFilePath = `posts/post${index}.html`;
-        await writeFile(`./public/${htmlFilePath}`, html);
-        // Update data object with path to the created html file
-        data[index]['path'] = `./${htmlFilePath}`;
-    }
+  for (let index = 0; index < data.length; index++) {
+    const html = htmlList[index];
+    const htmlFilePath = `posts/post${index}.html`;
+    await writeFile(`./public/${htmlFilePath}`, html);
+    // Update data object with path to the created html file
+    data[index].path = `./${htmlFilePath}`;
+  }
 
-    await embedContent(data);
-    console.log('finished embedding post data');
+  await embedContent(data);
+  console.log('finished embedding post data');
 }
 
 const HEADER_COMMENT = `/**
@@ -73,20 +73,18 @@ const HEADER_COMMENT = `/**
 */`;
 
 async function embedContent(data) {
-    const objects = data.map(content => {
-        return `{
-            fdate: '${content['fdate']}',
-            date: '${content['date']}',
-            author: '${content['author']}',
-            title: '${content['title']}',
-            path: '${content['path']}',
-        }`
-    });
-    const content = `export default [
-    ${objects.map(jsobj => jsobj + '')}
+  const objects = data.map((content) => `{
+            fdate: '${content.fdate}',
+            date: '${content.date}',
+            author: '${content.author}',
+            title: '${content.title}',
+            path: '${content.path}',
+        }`);
+  const content = `export default [
+    ${objects.map((jsobj) => `${jsobj}`)}
     ]`;
 
-    await writeFile('./src/posts.js', `${HEADER_COMMENT}\n\n${content}`);
+  await writeFile('./src/posts.js', `${HEADER_COMMENT}\n\n${content}`);
 }
 
 createHtmlFiles();
--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import posts from './posts';
 
 const postContainer = document.getElementById('posts');
 
-posts.forEach(post => {
-    const postEntry = document.createElement('div');
-    postEntry.setAttribute('id', 'post-entry');
+posts.forEach((post) => {
+  const postEntry = document.createElement('div');
+  postEntry.setAttribute('id', 'post-entry');
 
-    const postDateElement = document.createElement('span');
-    postDateElement.innerHTML = `${post['fdate']}`
-    postDateElement.setAttribute('id', 'post-date')
+  const postDateElement = document.createElement('span');
+  postDateElement.innerHTML = `${post.fdate}`;
+  postDateElement.setAttribute('id', 'post-date');
 
-    const postTitleElement = document.createElement('a');
-    postTitleElement.innerHTML = `${post['title']}`;
-    postTitleElement.href = post['path'];
-    postTitleElement.setAttribute('id', 'post-title')
+  const postTitleElement = document.createElement('a');
+  postTitleElement.innerHTML = `${post.title}`;
+  postTitleElement.href = post.path;
+  postTitleElement.setAttribute('id', 'post-title');
 
-    postEntry.appendChild(postDateElement);
-    postEntry.appendChild(postTitleElement);
-    postContainer.appendChild(postEntry);
-})
+  postEntry.appendChild(postDateElement);
+  postEntry.appendChild(postTitleElement);
+  postContainer.appendChild(postEntry);
+});
