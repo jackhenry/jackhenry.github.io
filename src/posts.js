9fe985f4000ad7bd60771d4450b896f307c811f6
Jack Henry <jack@jackerickson.net>
2022-03-20 20:29:43 -0500

    Bundled new pages build

--- src/posts.js
+++ src/posts.js
@@ -1,18 +1,20 @@
 /**
  * DO NOT DELETE OR MODIFY FILE.
  * This file is injected during the preprocessing stage.
- */
+*/
 
-export default [{
-    fdate: '2022-03-20',
-    date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
-    author: 'мырзам',
-    title: 'менің күнім',
-    path: './posts/post0.html',
-}, {
-    fdate: '2019-08-11',
-    date: 'Sun Aug 11 2019 13:45:30 GMT+0000 (Coordinated Universal Time)',
-    author: 'Jack Henry',
-    title: 'Test Markdown Document',
-    path: './posts/post1.html',
-}];
+export default [
+    {
+            fdate: '2022-03-20',
+            date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
+            author: 'мырзам',
+            title: 'менің күнім',
+            path: './posts/post0.html',
+        },{
+            fdate: '2019-08-11',
+            date: 'Sun Aug 11 2019 13:45:30 GMT+0000 (Coordinated Universal Time)',
+            author: 'Jack Henry',
+            title: 'Test Markdown Document',
+            path: './posts/post1.html',
+        }
+    ]
---
+++ src/projects.js
@@ -0,0 +1,111 @@
+/**
+ * DO NOT DELETE OR MODIFY FILE.
+ * This file is injected during the preprocessing stage.
+*/
+
+export default [
+`<div id="project-entry">
+  <div id="project-entry-line">
+    <span id="label">name:</span>
+    <span>
+      arna 
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">urls:</span>
+    <span class="multiple-links">
+      <a href=https://github.com/jackhenry/arna.git>repo</a>
+        <a href=https://jackhenry.github.io/arna>demo</a>
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">description:</span>
+    <span>A monorepo containing packages for generating, modifying, and rendering force directed graphs. Includes a custom DSL.
+</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">tools:</span>
+      <span>typescript</span>
+      <span>svelte+vite</span>
+      <span>chevrotain</span>
+  </div>
+</div>`,
+`<div id="project-entry">
+  <div id="project-entry-line">
+    <span id="label">name:</span>
+    <span>
+      rustntp 
+    </span>
+      <span id="in-progress">IN PROGRESS</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">urls:</span>
+    <span class="multiple-links">
+      <a href=https://github.com/jackhenry/rustntp.git>repo</a>
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">description:</span>
+    <span>Implementation of an SNTPv4 server/client in rust. The primary goal is to be compliant with RFC 4330.
+</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">tools:</span>
+      <span>rust</span>
+      <span>tokio</span>
+      <span>rustls</span>
+  </div>
+</div>`,
+`<div id="project-entry">
+  <div id="project-entry-line">
+    <span id="label">name:</span>
+    <span>
+      ESP-32 Chip-8 Emulator 
+    </span>
+      <span id="in-progress">IN PROGRESS</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">urls:</span>
+    <span class="multiple-links">
+      <a href=https://github.com/jackhenry/esp32-chip-8.git>repo</a>
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">description:</span>
+    <span>Chip-8 emulator cross compiled to run on ESP-32 embedded architecture.
+</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">tools:</span>
+      <span>C</span>
+      <span>ESP-IDF</span>
+  </div>
+</div>`,
+`<div id="project-entry">
+  <div id="project-entry-line">
+    <span id="label">name:</span>
+    <span>
+      jackhenry.github.io 
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">urls:</span>
+    <span class="multiple-links">
+      <a href=https://github.com/jackhenry/jackhenry.github.io.git>repo</a>
+        <a href=https://jackhenry.github.io/>demo</a>
+    </span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">description:</span>
+    <span>The same website you are currently browsing.  This site is bundled with no external dependencies.  The repo contains a very minimal &quot;static site generator&quot;  used to &quot;inject&quot; markdown files into bundled site.
+          
+</span>
+  </div>
+  <div id="project-entry-line">
+    <span id="label">tools:</span>
+      <span>javascript</span>
+      <span>webpack</span>
+      <span>node</span>
+  </div>
+</div>`,
+]
