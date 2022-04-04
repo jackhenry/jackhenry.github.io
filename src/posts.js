63c883cac57eb38f086c429c304dbeeb459e74df
Jack Henry <jack@jackerickson.net>
2022-04-04 18:45:50 -0500

    New pages bundle.

--- src/posts.js
+++ src/posts.js
@@ -5,19 +5,19 @@
 
 export default [
     {
+            fdate: '2022-04-04',
+            date: 'Mon Apr 04 2022 12:17:44 GMT+0000 (Coordinated Universal Time)',
+            author: 'Jack Henry',
+            title: 'Nondecreasing Monotonous Integers',
+            path: './posts/monotonic.html',
+            pin: false,
+        },{
             fdate: '2022-03-20',
             date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
             author: 'Jack Henry',
             title: 'about me',
             path: './posts/me.html',
             pin: true,
-        },{
-            fdate: '2022-03-20',
-            date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
-            author: 'мырзам',
-            title: 'ГҮЛ',
-            path: './posts/arna.html',
-            pin: false,
         },{
             fdate: '2022-03-20',
             date: 'Sun Mar 20 2022 00:17:44 GMT+0000 (Coordinated Universal Time)',
